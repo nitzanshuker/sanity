fe85bc5b3ace7f1d581b9362ac4bb0494ea77ed8
nitzanshuker <nitzanshuker@gmail.com>
2019-02-12 13:36:59 +0200

    +read me

--- models_sanity.rb
+++ models_sanity.rb
@@ -1,27 +1,61 @@
 
 require 'mongo'
-
+require 'csv'
+require 'mail'
+require 'uri'
+require 'net/http'
+require 'json'
 
 class ModelSanity
 
 
   def initialize
+
   p "yaron is awesome!!!"
-  end
-
-
-  def run(num)
-
-
-    client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'marketpulzz')
-
-    collection = :retailer_products
-    p client[collection].find
-
-    dn = client[collection].find({retailer_id:"324", deleted:false}, {:fields =>{name:1, product_type:1}}).limit(num)
-    dn.each { |doc| puts "#{doc['name']} , #{doc['product_type']}" }
 
   end
+
+
+def run(num)
+
+
+  client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'marketpulzz')
+
+  collection = :retailer_products
+  p client[collection].find
+
+  api_key = "QN3ErGYvmNznlOZXBcHgYQtt"
+
+  dn = client[collection].find({deleted:false}, {:fields =>{name:1, product_type:1}}).limit(num)
+  dn.each do |doc|
+    puts "#{doc['name']} , #{doc['product_type']}"
+    text, code = call_api(doc['name'], api_key)
+    puts text
+    
+  end
+end
+
+
+def call_api(text, api_key)
+
+  uri = URI("https://satool-test.mmuze.com/v1/conversations/")
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+  headers = {'Content-Type' => 'application/json' , 'X-Api-Key' => api_key}
+
+  data = {"text" => text}
+
+
+  response = https.post(uri.path,JSON.generate(data),headers)
+  response_text = response.body
+  response_text.force_encoding('UTF-8')
+  #response_text.gsub! /â„¢/, '™'
+
+  return [response_text, response.code]
+
+end
+
+
 
 end
 
@@ -32,8 +66,9 @@ if __FILE__ == $0
   num_from_user = ARGV[0].to_i
   qa_email  = ARGV[1]
   user_env = ARGV[2]
+  retailer_id = ARGV[3]
 
-  p num_from_user, qa_email, user_env
+  p num_from_user, qa_email, user_env, retailer_id
   nitzans_object.run(num_from_user)
 
 
