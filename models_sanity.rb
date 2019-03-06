c00aa614674baacca2d727f25ab7968275bbed7a
nitzanshuker <nitzanshuker@gmail.com>
2019-03-06 17:02:27 +0200

    +read me

--- models_sanity.rb
+++ models_sanity.rb
@@ -8,12 +8,27 @@ require 'json'
 
 class ModelSanity
 
+  RETAILER_ID_API_KEY_MAP = {
+
+      "324" => "QN3ErGYvmNznlOZXBcHgYQtt", # elc staging
+      "327" => "Tm9ATBDWYUrJVFQ4IiExQwtt", # elc production
+      "213" => "ewzTULzmuV1uGunELvojZQtt", # Shimona
+      "441" => "ccoSZU0qzMF1QHbumhazrwtt", # OP staging
+      "429" => "QL4imKcGa9wEVs9O4CW00wtt", # OP offical production
+      "272" => "trUw10iqq2I8N4RkxV28qQtt", # Shimon
+  }
+
+
+
 
   def initialize
 
   p "yaron is awesome!!!"
 
   end
+
+
+
 
   def model_parts(input)
 
@@ -31,6 +46,12 @@ class ModelSanity
     final_modles_array.push(base_word.join(" "))
 
     res.each_with_index do |item, index|
+      model_parts_combos = base_word.join(" ").downcase
+   # if model_parts_combos == @product_type
+    #   next
+    # else
+   #    p "no models for this product"
+   # end
       next if index <= 1
       base_word << item
       final_modles_array.push(base_word.join(" "))
@@ -38,29 +59,37 @@ class ModelSanity
     end
 
     return final_modles_array
-
-
   end
 
 
   def run(num)
 
 
+
+    CSV.open("/home/shuker/RubymineProjects/file.csv", "wb",
+             :write_headers=> true,
+             :headers => ["product name","models combination","pass?", "actual models"]) do |csv|
+
   client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'marketpulzz')
 
   collection = :retailer_products
   p client[collection].find
 
-  api_key = "QN3ErGYvmNznlOZXBcHgYQtt"
+  api_key = RETAILER_ID_API_KEY_MAP["324"]
+  #api_key = "QN3ErGYvmNznlOZXBcHgYQtt"
 
-  dn = client[collection].find({deleted:false}, {:fields =>{name:1, product_type:1}}).limit(num)
-  dn.each do |doc|
-    puts "#{doc['name']} , #{doc['product_type']}"
+  retailer_products_from_query = client[collection].find({deleted:false}, {:fields =>{name:1, product_type:1}}).limit(num)
+  retailer_products_from_query.each do |doc|
+    product_name = doc['name']
+    @product_type = doc['product_type']
+    puts "#{product_name} , #{doc['product_type']}"
 
-    hello, code = call_api(doc['name'], api_key)
+    hello, code = call_api(product_name, api_key)
     puts hello
 
-    all_phrases = model_parts(doc['name'])
+
+    all_phrases = model_parts(product_name)
+
     puts all_phrases
 
     all_phrases.each do |phrase|
@@ -69,26 +98,38 @@ class ModelSanity
       puts nlu
 
       models = models_in_the_nlu(nlu)
-      
+      puts models
+
+
+      # check if 'product_name' included in 'models'
+      if models.include?(product_name)
+          pass = true
+      else
+        pass = false
+      end
+      p pass
+
+      csv <<[product_name, phrase, pass, models]
     end
 
-    break
+      end
+  end
+
 
   end
 
-end
 
 
 def models_in_the_nlu(nlu)
   models_nlu = nlu["result"]["positive"]["models"] rescue []
   models_nlu.each do |model|
     puts model
+
   end
 
-
+  return models_nlu
 
 end
-
 
 
 def call_api(text, api_key)
@@ -106,12 +147,14 @@ def call_api(text, api_key)
   response_text.force_encoding('UTF-8')
   #response_text.gsub! /â„¢/, '™'
 
+
   return [response_text, response.code]
 
 end
 
-
 end
+
+
 
 if __FILE__ == $0
 
